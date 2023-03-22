916c70d15df3ccad2d28b864b8b5933d4b464577
PaulM <paulmicluta@gmail.com>
2023-03-22 06:12:25 +0200

    Container and Navigation component created

---
+++ src/app/Container/container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-container',
+  templateUrl: './container.component.html',
+  styleUrls: ['./container.ccomponent.css']
+})
+
+export class ContainerComponent{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ContainerComponent} from "./Container/container.component";
+import { NavigationComponent } from './navigation/navigation.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule
